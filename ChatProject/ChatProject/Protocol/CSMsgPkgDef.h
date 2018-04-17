ed9b5b370999f99573363ca59a0e74fc0cf7ffff
zzj <770230150@qq.com>
2018-04-17 21:17:12 +0800

    add

--- ChatProject/ChatProject/Protocol/CSMsgPkgDef.h
+++ ChatProject/ChatProject/Protocol/CSMsgPkgDef.h
@@ -1,36 +1,56 @@
 #pragma once
 
-#include "../ProtoDest/Chat.pb.h"
-#include <types.h>
+#include "./ProtoDest/Chat.pb.h"
+#include "../Util/Util.h"
 
 /*
-CS 消息包头定义
+	CS 消息包头定义
 */
 class CSMsgPkgHeadDef
 {
 public:
-	CSMsgPkgHeadDef();
-	~CSMsgPkgHeadDef();
+	CSMsgPkgHeadDef() : m_msgID(0), m_msgPkgLen(0) {}
+	~CSMsgPkgHeadDef() {}
 
-private:
+public:
 	uint	m_msgID;		// 消息ID
 	uint	m_msgPkgLen;	// 消息包体长度 
-
 };
 
-
 /*
-CS 消息包体定义
+	CS 消息包体定义
 */
+
 class CSMsgPkgBodyDef
 {
 public:
-	CSMsgPkgBodyDef();
-	~CSMsgPkgBodyDef();
+	CSMsgPkgBodyDef() 
+	{
+		m_csHeartBeatReq = NULL;
+		m_scHeartBeatRsp = NULL;
+		m_csLoginReq = NULL;
+		m_scLoginRsp = NULL;
+		m_csRegisterReq = NULL;
+		m_scRegisterRsp = NULL;
+	}
+	~CSMsgPkgBodyDef()
+	{
+		SAFE_DELETE(m_csHeartBeatReq);
+		SAFE_DELETE(m_scHeartBeatRsp);
+		SAFE_DELETE(m_csLoginReq);
+		SAFE_DELETE(m_scLoginRsp);
+		SAFE_DELETE(m_csRegisterReq);
+		SAFE_DELETE(m_scRegisterRsp);
+	}
 
-
+public:
+	CS_HEART_BEAT_REQ*	m_csHeartBeatReq;
+	SC_HEART_BEAT_RSP*	m_scHeartBeatRsp;
+	CS_LOGIN_REQ*		m_csLoginReq;
+	SC_LOGIN_RSP*		m_scLoginRsp;
+	CS_REGISTER_REQ*	m_csRegisterReq;
+	SC_REGISTER_RSP*	m_scRegisterRsp;
 };
-
 
 /*
 	CS 消息包定义
@@ -38,9 +58,11 @@ public:
 class CSMsgPkgDef
 {
 public:
-	CSMsgPkgDef();
-	~CSMsgPkgDef();
+	CSMsgPkgDef() {}
+	~CSMsgPkgDef() {}
 
-
+public:
+	CSMsgPkgHeadDef m_csMsgPkgHeadDef;	// 消息包头定义
+	CSMsgPkgBodyDef m_csMsgPkgBodyDef;	// 消息包体定义
 };
 
